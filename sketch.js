0165342eec851a6c3c75a8924f7875a809181a8b
KATHERINE YASBETH TENECELA MEJIA <katherine.tenecela@ucuenca.edu.ec>
2023-10-05 11:32:36 -0500

    pelota

--- sketch.js
+++ sketch.js
@@ -1,7 +1,39 @@
+let W1;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  Walker.setup();
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
+  Walker.draw();
+}
+
+class Walker {
+  constructor(_x, _y) {
+    this.posx = _x;
+    this.posy = _y;
+    this.vel = random(1, 10);
+    this.color = color(255, random(0, 200), random(0, 200));
+  }
+
+  update() {
+    this.posx += random(-this.vel, this.vel);
+    this.posy += random(-this.vel, this.vel);
+  }
+
+  display() {
+    fill(this.color);
+    noStroke();
+    circle(this.posx, this.posy, 10);
+  }
+
+  static setup() {
+    createCanvas(windowWidth, windowHeight);
+    W1 = new Walker(200, 200);
+  }
+
+  static draw() {
+    W1.update();
+    W1.display();
+  }
 }

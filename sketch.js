d9ab8965d762cad2fc23c211f6563ccad9ecaaf4
KATHERINE YASBETH TENECELA MEJIA <katherine.tenecela@ucuenca.edu.ec>
2023-10-20 21:51:16 -0500

    1.3 Random Walker

--- sketch.js
+++ sketch.js
@@ -1,45 +1,48 @@
-let pelota1;
+let w1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  pelota1 = new pelotarebota(windowWidth / 2, windowHeight * 0.2);
+  w1 = new Walker(200, 200);
 }
 
 function draw() {
-  pelota1.update();
-  pelota1.display();
+  w1.update();
+  w1.display();
 }
 
-class pelotarebota {
-  constructor(_posX, _posY) {
-    this.posX = _posX;
-    this.posY = _posY;
-    this.diam = random(10, 50);
-    this.rad = this.diam / 2;
-    this.esp = 50;
-    this.margen = 40;
-    this.piso = windowHeight - this.margen - this.esp / 2;
-    this.velY = 0;
-    this.acel = 0.98;
+class Walker {
+  constructor(_x, _y) {
+    this.posx = _x;
+    this.posy = _y;
+    this.vel = random(1, 50);
+    strokeWeight(5);
+    stroke(200, 200, 100);
+
+    this.color = color(150, random(10, 60), random(0, 220));
+
+    this.esCirculo = random() > 0.5 ? true : false;
+
+    // this.esCirculo;
+    // this.r = random();
+    // if (this.r > 0.5) {
+    // 	this.esCirculo = true;
+    // } else {
+    // 	this.esCirculo = false;
+    // }
   }
 
   update() {
-    this.velY += this.acel;
-    this.posY += this.velY;
-    if (this.posY >= this.piso - this.rad) {
-      this.posY = this.piso - this.rad;
-      this.velY *= -1;
-    }
+    this.posx += random(-this.vel, this.vel);
+    this.posy += random(-this.vel, this.vel);
   }
 
   display() {
-    background(0, 220, 210, 50);
-    noStroke();
-    rectMode(CENTER);
-
-    fill(255);
-    circle(this.posX, this.posY, this.diam);
-    fill(255, 0, 0);
-    rect(windowWidth / 2, windowHeight - this.margen, windowWidth, this.esp);
+    fill(this.color);
+    // noStroke();
+    if (this.esCirculo) {
+      circle(this.posx, this.posy, 60);
+    } else {
+      rect(this.posx, this.posy, 10, 10);
+    }
   }
 }
